f10079d4d24b5796ce5440063a6322c261ef696f
ddaa2000 <2470872965@qq.com>
2020-10-25 19:35:29 +0800

    Add clickme button and detail information

--- PKUGrouper/app/src/main/java/com/e/pkugrouper/MainActivity.java
+++ PKUGrouper/app/src/main/java/com/e/pkugrouper/MainActivity.java
@@ -1,6 +1,8 @@
 package com.e.pkugrouper;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 
 import android.os.Bundle;
 
@@ -10,5 +12,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        FragmentManager fm = getSupportFragmentManager();
+        Fragment fragment = fm.findFragmentById(R.id.main_frame);
+        if(fragment == null){
+             fragment = new HelloWorldFragment();
+             fm.beginTransaction().add(R.id.main_frame,fragment).commit();
+        }
+        else
+            fm.beginTransaction().replace(R.id.main_frame,fragment).commit();
     }
 }
