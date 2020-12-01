0bdd587974d8475804cafbcdca3bb1b9cbcfc452
Kliar <1800013014@pku.edu.cn>
2020-12-01 11:36:49 +0800

    add content of JSON and add safety check, still untested and uncompleted

--- PKUGrouper/app/src/main/java/com/e/pkugrouper/Managers/MessageManager.java
+++ PKUGrouper/app/src/main/java/com/e/pkugrouper/Managers/MessageManager.java
@@ -20,35 +20,82 @@ public class MessageManager extends HttpManager implements IMessageManager{
 
     @Override
     public List<IMessage> getCurrentUserMessages() {
+        //判断currentUser是否存在
+        if(currentUser == null){
+            //report or throw exception
+            return false;
+        }
+
+        //得到currentUser的信息
         String url = "/message";
         List<String> parameters = [currentUser.getUserID()];
-        String Message = self.httpGet(url, parameters, null);
+        String Message_JSON_All = self.httpGet(url, parameters, null);
+
+        //如果得不到信息
+        if(Message_JSON_All == null){
+            //report
+            return null;
+        }
+
+        //下一步是分割的问题 暂时得不到解决
+        List<String> Message_JSON_List;
+        List<IMessage> Message_List;
+        for (String Message_JSON: Message_JSON_List){
+            IMessage current_message = new IMessage;
+            current_message.loadFromJSON(Message_JSON);
+            Message_List.add(current_message);
+        }
+        //return Message_List;
         return null;
     }
 
     @Override
     public boolean reportBug(IMessage bug) {
-        String url = "/message/bug";
+        //检查currentUser
         if(currentUser == null){
             //throw
             return false;
         }
+
+        //报告bug
+        String url = "/message/bug";
         List<String> parameters = [currentUser.getUserID()];
-        String bug_content = bug.getMessageContent();
-        String bug_list = self.httpPost(url, parameters, bug_content);
+        String bug_JSON = bug.toJSON();
+        String bug_final_JSON = self.httpPost(url, parameters, bug_JSON);
+
+        //检查是否报告成功
+        if(bug_final_JSON == null){
+            //report or throw exception
+            return false;
+        }
+        else{
+            return true;
+        }
         return false;
     }
 
     @Override
     public boolean report(IMessage report) {
-        String url = "/message/report";
+        //检查currentUser
         if(currentUser == null){
             //throw
             return false;
         }
+
+        //报告
+        String url = "/message/report";
         List<String> parameters = [currentUser.getUserID()];
-        String report_content = report.getMessageContent();
-        String report_list = self.httpPost(url, parameters, report_content);
+        String report_JSON = report.toJSON();
+        String report_final_JSON = self.httpPost(url, parameters, report_JSON);
+
+        //检查是否报告成功
+        if(report_final_JSON == null){
+            //report or throw exception
+            return false;
+        }
+        else {
+            return true;
+        }
         return false;
     }
 
--- PKUGrouper/app/src/main/java/com/e/pkugrouper/Managers/UserManager.java
+++ PKUGrouper/app/src/main/java/com/e/pkugrouper/Managers/UserManager.java
@@ -11,44 +11,109 @@ public class UserManager extends HttpManager implements IUserManager{
     private IUser user;
     @Override
     public ICommonUser findMemberByID(int missionID, int memberID) {
-        List<String> parameters = [String.ValueOf(missionID), String.ValueOf(memberID)];
+        if(user == null) {
+            //throw;
+            return null;
+        } //检测user对象是否存在
+
+        //读取要查找的member的JSON序列
+        List<String> parameters = [user.getUserID(), String.ValueOf(missionID),
+                String.ValueOf(memberID)];
         String url = "/user/member";
-        String UID = self.httpGet(url, parameters, null);
-        if (UID == null) {
+        String Member_JSON = self.httpGet(url, parameters, null);
+
+        //如果该member查找不到
+        if (Member_JSON == null) {
+            //not found
             return null;
         }
+        //生成member对应的ICommonUser对象
         else {
+            ICommonUser member = new ICommonUser;
+            member.loadFromJSON(Member_JSON);
+            //return member;
             return null;
         }
     }
 
     @Override
-    public ICommonUser getSelf(int userID) {
+    public ICommonUser getSelf() {
+        //根据给定的userID生成对象
         List<String> parameters = [String.ValueOf(userID)];
         String url = "/user/self";
-        String UID = user.getUserID();
-        String User_Info = self.httpGet(url, parameters, null);
+        String User_JSON = self.httpGet(url, parameters, null);
+
+        //从返回的JSON字符串中得到当前使用的User对象
+        ICommonUser Self = new ICommonUser;
+        Self.loadFromJSON(User_JSON);
+        //return Self;
         return null;
     }
 
     @Override
     public IUser userLogIn(IUser currentUser) {
+        //生成登录产生的JSON字符串
         String url = "/user/login";
-        self.httpPost(url, null, null);
+        String User_JSON = currentUser.toJSON();
+        String User_Login_JSON = self.httpPost(url, null, User_JSON);
+
+        //如果返回JSON字符串为空 认为登录失败
+        if(User_Login_JSON == null){
+            //report login failed
+            return null;
+        }
+
+        //生成登陆后的user对象
+        IUser User_Login = new IUser;
+        User_Login.loadFromJSON(User_Login_JSON);
+        user = User_Login;
+
+        //更新messageManager和missionMagager中的currentUser
+        if(messageManager == null){
+            return null;
+        }
+        messageManager.setCurrentUser(user);
+
+        if(missionManager == null) {
+            return null
+        }
+        missionManager.setCurrentUser(user);
+        //return User_Login;
         return null;
     }
 
     @Override
     public IUser userRegister(IUser currentUser) {
+        //生成注册产生的JSON字符串
         String url = "/user/register";
-        self.httpPost(url, null, null);
+        String User_JSON = currentUser.toJSON();
+        String User_Register_JSON = self.httpPost(url, null, User_JSON);
+
+        //生成注册后的User对象
+        IUser User_Register = new IUser;
+        User_Register.loadFromJSON(User_Register_JSON);
+        user = User_Register;
+
+        //更新messageManager和missionManager的currentUser
+        if(messageManager == null){
+            return null;
+        }
+        messageManager.setCurrentUser(user);
+
+        if(missionManager == null) {
+            return null
+        }
+        missionManager.setCurrentUser(user);
+        //return User_Register;
         return null;
     }
 
     @Override
     public boolean setMissionManager(IMissionManager _missionManager) {
-        if (_missionManager == null)
+        if (_missionManager == null) {
+            //throw exception
             return false;
+        }
         else{
             missionManager = _missionManager;
             return true;
@@ -57,8 +122,10 @@ public class UserManager extends HttpManager implements IUserManager{
 
     @Override
     public boolean setMessageManager(IMessageManager _messageManager) {
-        if (_messageManager == null)
+        if (_messageManager == null) {
+            //throw exception;
             return false;
+        }
         else{
             messageManager = _messageManager;
             return true;
@@ -67,35 +134,110 @@ public class UserManager extends HttpManager implements IUserManager{
 
     @Override
     public boolean editInfo() {
+        //判断user是否存在
+        if(user == null){
+            //report or throw exception
+            return false;
+        }
+
+        //用Put修改用户的信息
         String url = "/user/info";
         List<String> parameters = [user.getUserID()];
-        String body = null;
-        String EditInfo = self.httpPut(url, parameters, body);
+        String User_JSON = user.toJSON();
+        String EditInfo_JSON = self.httpPut(url, parameters, User_JSON);
+
+        //如果成功修改了信息，用修改后返回的JSON更新当前用户的信息
+        if(EditInfo_JSON != null) {
+            user.loadFronJSON(EditInfo_JSON);
+            return true;
+        }
+        else {
+            //report or throw exception
+            return false;
+        }
     }
 
     @Override
     public boolean editTags() {
+        //判断user是否存在
+        if(user == null){
+            //report or throw exception
+            return false;
+        }
+
+        //修改用户的Tags
         String url = "/user/tags";
         List<String> parameters = [user.getUserID()];
-        String taglist = user.getTags();
-        String tags = self.httpPut(url, parameters, taglist);
+        List<String> tags = user.getTags();
+        String taglist = "";
+        for (String tag: tags){
+            taglist += tag + " ";
+        }
+        String tags_JSON = self.httpPut(url, parameters, taglist);
+
+        //如果成功修改了tags，用修改后返回的JSON更新当前用户的tags
+        if(tags_JSON != null) {
+            user.loadFromJSON(tags_JSON);
+            return true;
+        }
+        else{
+            //report or throw exception
+            return false;
+        }
     }
 
     @Override
     public boolean changePassword() {
+        //判断user是否存在
+        if(user == null){
+            //report or throw exception
+            return false;
+        }
+
+        //修改用户的password
         String url = '/user/code';
         List<String> parameters = [user.getUserID()];
         String Password = null;
-        String new_password = self.httpPut(url, parameters, Password);
+        String password_JSON = self.httpPut(url, parameters, Password);
+
+        //如果成功修改了password，用修改后返回的JSON更新当前用户的password
+        //目前得到用户的password和把修改后的password加载到用户对象 都不明确
+        if(password_JSON != null) {
+            user.loadFromJSON(password_JSON);
+            return true;
+        }
+        else{
+            //report or throw exception
+            return false;
+        }
     }
 
     @Override
     public boolean evaluate(int missionID, int evaluateeID, int score) {
-        List<String> parameters = [String.ValueOf(missionID), String.ValueOf(evaluateeID),
-            String.ValueOf(score)];
-        String evaluation = null;
+        //判断user是否存在
+        if(user == null){
+            //report or throw exception
+            return false;
+        }
+
         String url = "/user/evalute";
-        self.httpPost(url, parameters, evaluation);
-        return false;
+        List<String> parameters = [user.getUserID(), String.ValueOf(missionID),
+                String.ValueOf(evaluateeID)];
+
+        Evaluation evaluation = new Evaluation;
+        evaluation.setEvaluateeID(evaluateeID);
+        evaluation.setMissionID(missionID);
+        evaluation.setScore(score);
+        String evaluation_JSON = evaluation.toJSON();
+
+        String evaluate_JSON = self.httpPost(url, parameters, evaluation_JSON);
+        if(evaluate_JSON != null){
+            evaluation.loadFromJSON(evaluate_JSON);
+            return true;
+        }
+        else {
+            // report or throw exception
+            return false;
+        }
     }
 }
