73d6a1ef1b048db012f7d4624de86fd43628c536
KugaMashiro <1800012987@pku.edu.cn>
2020-12-10 18:49:30 +0800

    basically finished

--- PKUGrouper/app/src/main/java/com/e/pkugrouper/Models/IEvaluation.java
+++ PKUGrouper/app/src/main/java/com/e/pkugrouper/Models/IEvaluation.java
@@ -1,13 +1,16 @@
 package com.e.pkugrouper.Models;
 
 public interface IEvaluation extends ISerializable{
-    int getEvaluatorID();
+	int getEvaluationID();
+    int getEvaluaterID();
     int getEvaluateeID();
     int getMissionID();
     double getScore();
-    void setEvaluationID(int evaluationID);
-    void setEvaluateeID(int evaluateeID);
-    void setMissionID(int missionID);
-    void setScore(double score);
-    void setEvaluatorID(int evaluatorID);
+    void setEvaluationID(int _evaluationID);
+    void setEvaluateeID(int _evaluateeID);
+    void setMissionID(int _missionID);
+    void setEvaluaterID(int _evaluaterID);
+    void setScore(double _score);
+    String getTimeStamp();
+    void setTimeStamp(String _timeStamp);
 }
--- PKUGrouper/app/src/main/java/com/e/pkugrouper/Models/User.java
+++ PKUGrouper/app/src/main/java/com/e/pkugrouper/Models/User.java
@@ -1,94 +1,147 @@
 package com.e.pkugrouper.Models;
 
 import java.util.List;
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
 
 public class User implements IUser{
-    private int UserID = 0;
+    private String mailBox;
+    private String userName;
+    private int userID;
     private String password;
-    private String Username;
-    private String tele;
+    private String contactInformation;
+    private double averageScore;
+
+
+    private List<Integer> missionIDs;
+    private List<Integer> messageIDs;
+    private List<Integer> violationIDs;
+    private List<Integer> evaluationIDs;
+
     @Override
     public String getUserName() {
-        return Username;
+        return userName;
     }
 
     @Override
-    public void setUserName(String username) {
-        Username = username;
+    public void setUserName(String _userName) {
+        userName=_userName;
     }
 
+    
+    
+    @Override
+    public String getPassword() {
+    	return password;
+    }
+    
+    @Override
+    public void setPassword(String _password) {
+    	password=_password;
+    }
+   
+    
     @Override
     public String getMailBox() {
-        return null;
+        return mailBox;
     }
 
     @Override
     public void setMailBox(String _mailBox) {
-
-    }
-
-    @Override
-    public String getTele() {
-        return tele;
-    }
-
-    @Override
-    public void setTele(String Tele) {
-        tele = Tele;
+        mailBox=_mailBox;
     }
 
     @Override
     public int getUserID() {
-        return UserID;
+        return userID;
     }
 
     @Override
     public void setUserID(int _userID) {
-        UserID = _userID;
+        userID=_userID;
     }
 
     @Override
-    public String getPassword() {
-        return password;
+    public String getContactInformation() {
+        return contactInformation;
     }
 
     @Override
-    public IIdentification getIdentification() {
-        return null;
-    }
-
-    @Override
-    public void setIdentification(IIdentification _identification) {
-
+    public void setContactInformation(String _contactInformation) {
+        contactInformation=_contactInformation;
     }
 
     @Override
     public List<Integer> getMessageIDs() {
-        return null;
+        return messageIDs;
     }
 
     @Override
     public List<Integer> getEvaluationIDs() {
-        return null;
+        return evaluationIDs;
     }
 
     @Override
-    public List<String> getTags() {
-        return null;
+    public List<Integer> getViolationIDs() {
+        return  violationIDs;
     }
 
     @Override
-    public void setPassword(String _password) {
-        password = _password;
+    public List<Integer> getMissionIDs() {
+        return missionIDs;
     }
+
+    @Override
+    public double getAverageScore(){
+        return averageScore;
+    }
+
+    @Override
+    public void setAverageScore(double _averageScore){
+        averageScore=_averageScore;
+    }
+
+    @Override
+    public void setMessageIDs(List<Integer> _messageIDs) {
+    	messageIDs=_messageIDs;
+    }
+    @Override
+    public void setEvaluationIDs(List<Integer> _evaluationIDs) {
+    	evaluationIDs=_evaluationIDs;
+    }
+    @Override
+    public void setMissionIDs(List<Integer> _missionIDs) {
+    	missionIDs=_missionIDs;
+    }
+    @Override
+    public void setViolationIDs(List<Integer> _violationIDs) {
+    	violationIDs=_violationIDs;
+    }
+
 
     @Override
     public String toJSON() {
-        return null;
+        JSONObject object=new JSONObject();
+
+        object.put("mailbox",mailBox);
+        object.put("missionIDs",missionIDs);
+        object.put("evaluationIDs",evaluationIDs);
+        object.put("violationIDs",violationIDs);
+        object.put("averageScore",averageScore);
+
+
+        String objStr=JSON.toJSONString(object);
+        return objStr;
     }
 
     @Override
     public void loadFromJSON(String JSONString) {
+        JSONObject object=JSON.parseObject(JSONString);
 
+        mailBox=object.getString("mailbox");
+        missionIDs=JSON.parseArray(object.getJSONArray("missionIDs").toJSONString(),Integer.class);
+        evaluationIDs=JSON.parseArray(object.getJSONArray("evaluationIDs").toJSONString(),Integer.class);
+        violationIDs=JSON.parseArray(object.getJSONArray("violationIDs").toJSONString(),Integer.class);
+        averageScore=object.getDoubleValue("averageScore");
     }
 }
