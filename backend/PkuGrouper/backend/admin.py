b3ee8eb290ee2727947e9db0c9dd0abd9320bf62
ppppaidy <ppppaidy@pku.edu.cn>
2020-12-15 08:27:17 +0800

    update admin.py, models.py

--- backend/PkuGrouper/backend/admin.py
+++ backend/PkuGrouper/backend/admin.py
@@ -4,14 +4,58 @@ from django.contrib import admin
 
 from .models import *
 
-admin.site.register(User)
-admin.site.register(Mission)
-admin.site.register(Membership)
-admin.site.register(Applicantship)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('mailbox', 'id')
+
+admin.site.register(User, UserAdmin)
+
+class MissionAdmin(admin.ModelAdmin):
+    list_display = ('title', 'id', 'showPublishTime')
+
+admin.site.register(Mission, MissionAdmin)
+
+class MembershipAdmin(admin.ModelAdmin):
+    list_display = ('mission', 'user')
+admin.site.register(Membership, MembershipAdmin)
+
+class ApplicantshipAdmin(admin.ModelAdmin):
+    list_display = ('mission', 'user')
+    
+admin.site.register(Applicantship, ApplicantshipAdmin)
 admin.site.register(Channel)
-admin.site.register(MissionChannelship)
-admin.site.register(Evaluation)
-admin.site.register(Message)
-admin.site.register(Receivership)
-admin.site.register(Reporteeship)
-admin.site.register(Captcha)
+
+class MissionChannelshipAdmin(admin.ModelAdmin):
+    list_display = ('mission', 'channel')
+admin.site.register(MissionChannelship, MissionChannelshipAdmin)
+
+class EvaluationAdmin(admin.ModelAdmin):
+    list_display = ('id',
+                    'evaluater',
+                    'evaluatee',
+                    'showTimeStamp',
+                    'evaluationScore')
+admin.site.register(Evaluation, EvaluationAdmin)
+
+class MessageAdmin(admin.ModelAdmin):
+    list_display = ('id',
+                    'messageType',
+                    'messageContent',
+                    'showTimeStamp',
+                    'publisher')
+
+admin.site.register(Message, MessageAdmin)
+
+class ReceivershipAdmin(admin.ModelAdmin):
+    list_display = ('message', 'user')
+
+admin.site.register(Receivership, ReceivershipAdmin)
+
+class ReporteeshipAdmin(admin.ModelAdmin):
+    list_display = ('message', 'user')
+
+admin.site.register(Reporteeship, ReporteeshipAdmin)
+
+class CaptchaAdmin(admin.ModelAdmin):
+    list_display = ('mailbox', 'timeStamp')
+
+admin.site.register(Captcha, CaptchaAdmin)
--- backend/PkuGrouper/backend/models.py
+++ backend/PkuGrouper/backend/models.py
@@ -2,13 +2,19 @@ from django.db import models
 
 # Create your models here.
 
+def fixtime(s):
+    x = s.find('.')
+    if x == -1:
+        return s
+    return s[:x]
+
 class User(models.Model):
     mailbox = models.EmailField()
     username = models.CharField(max_length=200)
     tele = models.CharField(max_length=200)
     passwordAfterMD5 = models.CharField(max_length=200)
     def __str__(self):
-        return self.mailbox
+        return str(self.id)
 
 class Mission(models.Model):
     title = models.CharField(max_length=200)
@@ -24,7 +30,9 @@ class Mission(models.Model):
     applicants = models.ManyToManyField(User, through='Applicantship',
                                         related_name='missionsAsApplicant')
     def __str__(self):
-        return self.title
+        return str(self.id)
+    def showPublishTime(self):
+        return fixtime(str(self.publishTime))
 
 class Membership(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
@@ -54,6 +62,8 @@ class Evaluation(models.Model):
                                   related_name='evaluationsAsEvaluatee')
     mission = models.ForeignKey(Mission, on_delete=models.CASCADE,
                                 related_name='evaluationsOfTheMission')
+    def showTimeStamp(self):
+        return fixtime(str(self.timeStamp))
 
 class Message(models.Model):
     messageContent = models.CharField(max_length=1000)
@@ -67,7 +77,9 @@ class Message(models.Model):
     reportees = models.ManyToManyField(User, through='Reporteeship',
                                       related_name='messagesAsReportee')
     def __str__(self):
-        return self.messageContent
+        return str(self.id)
+    def showTimeStamp(self):
+        return fixtime(str(self.timeStamp))
 
 class Receivership(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
